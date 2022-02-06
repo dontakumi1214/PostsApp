72f5204a8b9151a00ee561afd9a09bb0a1937f43
k-suke39 <pz.mozarla@gmail.com>
2022-02-06 14:02:33 +0000

    create:commentsテーブルのマイグレーションとシーダー作成

---
+++ database/migrations/2022_02_06_064920_create_comments_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('comment', 255);
+            $table->biginteger('user_id')->unsigned();
+            $table->biginteger('post_id')->unsigned();
+            $table->timestamps();
+            
+            //$table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            //$table->foreign('post_id')->references('id')->on('posts')->onDelete('cascade');
+        });
+    }
+
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comments');
+    }
+}
