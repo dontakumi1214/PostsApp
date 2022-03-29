09f1953e55974ed46a8522381e1d039f1a323154
dontakumi1214 <condotakumi@gmail.com>
2022-03-29 13:02:00 +0000

    全体修正

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -5,13 +5,9 @@
         <div class="user-card col-md-12">
             <div class="text-center">
                 <i class="fas fa-user-circle fa-3x"></i>
-                <h3>
-                　{{ $user->name }}
-　　　　         </h3>
+                <h3>{{ $user->name }}</h3>
                 <div class="col-12">
-                    <p>
-                      {{ $user->email }}
-                    </p>
+                    <p>{{ $user->email }}</p>
                 </div>
                 <div class="col-12 mt-3">
                     <a class="btn btn-outline-dark common-btn btn-sm edit-profile-btn" href="{{route('users.edit', ['id' => Auth::id()])}}"><i class="fas fa-user-edit"></i>プロフィール編集
